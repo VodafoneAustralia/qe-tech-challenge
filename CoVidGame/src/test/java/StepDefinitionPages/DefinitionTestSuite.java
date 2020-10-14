a366615990b53316216deceb1061d4cafbf9b0c9
nandadeep <pabrekarnd33@gmail.com>
2020-10-15 00:43:22 +1100

    Update DefinitionTestSuite.java
    Remove commented out code

--- CoVidGame/src/test/java/StepDefinitionPages/DefinitionTestSuite.java
+++ CoVidGame/src/test/java/StepDefinitionPages/DefinitionTestSuite.java
@@ -4,17 +4,7 @@ import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
-//@CucumberOptions(tags = "@scen")
-//@WithTags({
-//        @WithTag("scen")
-//})
+
 @CucumberOptions(features="src/test/resources/features/Regression/Components/")
-//@CucumberOptions(features="src/test/resources/features/Regression/Components/", tags = {"Regression"})
-//@RunWith(Cucumber.class)
-//@CucumberOptions(features = { "classpath:features/*.feature" }, glue = "StepDefinitionFSO", plugin = {
-//        "pretty:target/prettyReport.txt", "html:target/cucumber", "json:target/cucumber.json", "rerun:target/rerun.txt",
-//        "junit:target/junit-report.xml", "testng:target/testng-output.xml" }, monochrome = true, tags = {"~@cleanup"})
-//public class FeatureRunnerTest {
-//
-//}
+
 public class DefinitionTestSuite {}
